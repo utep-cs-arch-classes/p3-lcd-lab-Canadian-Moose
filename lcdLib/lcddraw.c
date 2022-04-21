31ac9775be213415f9a75aa62fd1839dfdb7bcb0
Canadian-Moose <89561054+Canadian-Moose@users.noreply.github.com>
2022-04-20 18:36:52 -0600

    Update lcddraw.c

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -137,7 +137,7 @@ void drawChar11x16(u_char rcol, u_char rrow, char c,
   u_char oc = c - 0x20;
 
   lcd_setArea(rcol, rrow, rcol + 10, rrow + 15); /* relative to requested col/row */
-  while (row < 8) {
+  while (row < 17) {
     while (col < 12) {
       u_int colorBGR = (font_11x16[oc][col] & bit) ? fgColorBGR : bgColorBGR;
       lcd_writeColor(colorBGR);
