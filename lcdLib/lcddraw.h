3810c51172132c3a6bb47d6f8b62f720ac6784a4
Canadian-Moose <89561054+Canadian-Moose@users.noreply.github.com>
2022-04-20 18:15:34 -0600

    Adding other font sizes

--- lcdLib/lcddraw.h
+++ lcdLib/lcddraw.h
@@ -50,6 +50,16 @@ void drawString5x7(u_char col, u_char row, char *string,
 void drawChar5x7(u_char col, u_char row, char c, 
 		 u_int fgColorBGR, u_int bgColorBGR);
 
+void drawChar8x12(u_char rcol, u_char rrow, char c, 
+     u_int fgColorBGR, u_int bgColorBGR);
+void drawString8x12(u_char col, u_char row, char *string,
+		u_int fgColorBGR, u_int bgColorBGR);
+
+void drawChar11x16(u_char rcol, u_char rrow, char c, 
+     u_int fgColorBGR, u_int bgColorBGR);
+void drawString11x16(u_char col, u_char row, char *string,
+		u_int fgColorBGR, u_int bgColorBGR);
+
 /** Draw rectangle outline
  *  
  *  \param colMin Column start
