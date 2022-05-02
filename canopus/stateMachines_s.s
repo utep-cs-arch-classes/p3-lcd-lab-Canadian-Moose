5ccdb7be2023a509d2ea057ea1134dfa9ac383b0
Canadian-Moose <89561054+Canadian-Moose@users.noreply.github.com>
2022-05-02 16:49:44 -0600

    Update stateMachines_s.s

--- canopus/stateMachines_s.s
+++ canopus/stateMachines_s.s
@@ -26,8 +26,8 @@ chooseState:
 		        .word case_1
 		        .word case_2
 	
-	#cmp &state, #2
-	#jn default
+	cmp &state, #2
+	jl default
 	
 	mov.w &state, r12
 	add.w r12, r12
@@ -43,9 +43,9 @@ chooseState:
 	case_2:
 		CALL switch_interrupt_handler
 		CALL game_over
-		#jmp end_state
-	#default:
-		#CALL play_game
+		jmp end_state
+	default:
+		CALL play_game
 	
 	end_state: 
 	pop r0
