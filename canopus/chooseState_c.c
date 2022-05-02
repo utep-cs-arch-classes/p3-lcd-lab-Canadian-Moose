0af41bcb5067051a2c834e51667264aaa048c872
Canadian-Moose <89561054+Canadian-Moose@users.noreply.github.com>
2022-05-02 17:06:12 -0600

    Moving chooseState to it's own file

--- canopus/chooseState_c.c
+++ canopus/chooseState_c.c
@@ -1,1 +1,20 @@
+#include <msp430.h>
+#include "stateMachines.h"
 
+void chooseState(void){
+  switch(state){
+    case 0: 
+      play_game();
+      break;
+    case 1:
+      switch_interrupt_handler();
+      you_win();
+      break;
+    case 2:
+      switch_interrupt_handler();
+      game_over();
+      break;
+    default:
+      play_game();
+    }
+}
