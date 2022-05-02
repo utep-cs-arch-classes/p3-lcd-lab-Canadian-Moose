f43eb5938cf1569ae004c0c6096d9a39844bb58b
Canadian-Moose <89561054+Canadian-Moose@users.noreply.github.com>
2022-05-02 17:03:09 -0600

    Moving chooseState to own file

---
+++ canopus/chooseState_s.s
@@ -0,0 +1,38 @@
+
+	.arch msp430g2553
+	.p2align 1,0
+	.text
+	.global chooseState
+	
+# variables 
+.extern  state
+
+# void chooseState(void)
+chooseState: 
+jump_table:  .word case_0
+	.word case_1
+	.word case_2
+	
+	cmp &state, #2
+	jl default
+	
+	mov.w &state, r12
+	add.w r12, r12
+	mov.w jump_table(r12), pc
+	
+case_0:
+	CALL play_game
+	jmp end_state
+case_1: 
+	CALL switch_interrupt_handler
+	CALL you_win
+	jmp end_state
+case_2:
+	CALL switch_interrupt_handler
+	CALL game_over
+	jmp end_state
+default:
+	CALL play_game
+	
+end_state: 
+pop r0
