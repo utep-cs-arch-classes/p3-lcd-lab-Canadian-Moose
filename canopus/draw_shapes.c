d2dec4ce5bc2835e11f07e7116d98e9a0187975c
Canadian-Moose <89561054+Canadian-Moose@users.noreply.github.com>
2022-05-01 18:29:25 -0600

    Update draw_shapes.c

--- canopus/draw_shapes.c
+++ canopus/draw_shapes.c
@@ -278,6 +278,8 @@ void drawWinScreen(void){
   drawString5x7(5, 139, " continue...", COLOR_RED, COLOR_GREEN);
   if (countdown % 2 == 0){
     buzzer_set_period(10000);
+  }else{
+    buzzer_set_period(0);
   }
 }
 
