839245447e455de17dfe9168fdfda6c252322e83
Canadian-Moose <89561054+Canadian-Moose@users.noreply.github.com>
2022-05-04 21:01:07 -0600

    Update lcddraw.c

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -105,10 +105,10 @@ void drawChar8x12(u_char rcol, u_char rrow, char c,
   u_char bit = 0x01;
   u_char oc = c - 0x20;
 
-  lcd_setArea(rcol, rrow, rcol + 7, rrow + 11); /* relative to requested col/row */
+  lcd_setArea(rcol, rrow, rcol + 7, rrow + 12); /* relative to requested col/row */
   while (row < 13) {
     while (col < 8) {
-      u_int colorBGR = (font_8x12[col][oc] & bit) ? fgColorBGR : bgColorBGR;
+      u_int colorBGR = (font_8x12[oc][col] & bit) ? fgColorBGR : bgColorBGR;
       lcd_writeColor(colorBGR);
       col++;
     }
